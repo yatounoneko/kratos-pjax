9c8ad4bf5ed31d9138acffc070d39a97a3db67c3
Yatounoneko <22129910+yatounoneko@users.noreply.github.com>
2026-01-09 20:06:31 +0800

    Add enqueue function
    For fake scrollbar styles and scripts

---
+++ inc/enqueue.php
@@ -0,0 +1,20 @@
+<?php
+function enqueue_fake_scrollbar() {
+    if (!is_admin()) {
+        wp_enqueue_style(
+            'fake-scrollbar-css',
+            get_stylesheet_directory_uri() . '/static/css/fake-scrollbar.css',
+            array(),
+            '1.0.13'
+        );
+
+        wp_enqueue_script(
+            'fake-scrollbar-js',
+            get_stylesheet_directory_uri() . '/static/js/fake-scrollbar.js',
+            array(),
+            '1.0.13',
+            true
+        );
+    }
+}
+add_action('wp_enqueue_scripts', 'enqueue_fake_scrollbar');
