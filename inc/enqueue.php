1483230c25458f61115ea5dcc71071a6f43d570b
Yatounoneko <22129910+yatounoneko@users.noreply.github.com>
2026-01-09 20:17:25 +0800

    Change enqueue paths to use get_template_directory_uri

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -3,14 +3,14 @@ function enqueue_fake_scrollbar() {
     if (!is_admin()) {
         wp_enqueue_style(
             'fake-scrollbar-css',
-            get_stylesheet_directory_uri() . '/static/css/fake-scrollbar.css',
+            get_template_directory_uri() . '/static/css/fake-scrollbar.css',
             array(),
             '1.0.13'
         );
 
         wp_enqueue_script(
             'fake-scrollbar-js',
-            get_stylesheet_directory_uri() . '/static/js/fake-scrollbar.js',
+            get_template_directory_uri() . '/static/js/fake-scrollbar.js',
             array(),
             '1.0.13',
             true
